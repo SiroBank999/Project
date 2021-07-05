f1973b221ea23216bae5fce2eeab2219b497eec9
SiroBank999 <SiroBank999@gmail.com>
2021-07-05 17:58:11 +0700

    Database

---
+++ Client_Project/src/Service/Database.java
@@ -0,0 +1,24 @@
+package Service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+	private static String DATABASE_LINK ="jdbc:sqlserver://localhost:1433;databaseName=csdl_banhang";
+	private static String USER_NAME = "sa";
+    private static String PASSWORD = "123456";
+    public static Connection Connect() {
+    	Connection conn =null;
+    	try {
+    		 conn = DriverManager.getConnection(DATABASE_LINK,USER_NAME,PASSWORD);
+			if(conn != null) {
+				System.out.print("Kết nối thành công");
+			}
+		}catch (Exception e) {
+			System.out.print("Không thể kết nối ");
+			e.printStackTrace();
+		}
+    	return conn;
+    }
+
+}
