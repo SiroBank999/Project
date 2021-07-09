cafbfd5ff674759cffbebc703473d27bb7a818d8
SiroBank999 <SiroBank999@gmail.com>
2021-07-09 23:13:52 +0700

    GIO HANG

--- Client_Project/src/Service/Size_service.java
+++ Client_Project/src/Service/Size_service.java
@@ -31,4 +31,40 @@ public class Size_service {
 		}
 		return list;
 	}
+	public Size getSizeById(int id) {
+		String sql = "select * from size WHERE id = ?";
+		Size size = new Size();
+		try {
+			conn = Database.Connect();
+			ps = conn.prepareStatement(sql);
+			ps.setInt(1, id);
+			rs = ps.executeQuery();
+			while (rs.next()) {
+				
+				size.setId(rs.getInt("id"));
+				size.setName(rs.getString("name"));
+				
+			}
+		} catch (Exception e) {
+
+		}
+		return size;
+	}
+	public static void main(String[] args) {
+		Size_service ss = new Size_service();
+		List<Size> list =ss.Size();
+		String size ="M";
+		String tmp =null;
+		int id_tmp =0;
+		int id = 0;
+		for(Size s:list) {
+			tmp =s.getName();
+			id =s.getId();
+			if(tmp.equals(size)) {
+				id_tmp = id;
+			}
+			System.out.print(id+"\n");
+		}
+		System.out.print(id_tmp);
+	}
 }
