41f6e7d467f4027a94b698d82e858ace1e4b3320
khanhquoc2920 <khanhquoc2920@gmail.com>
2021-07-14 21:22:54 +0700

    giao dien checkout

---
+++ Client_Project/WebContent/templates/js/checkout.js
@@ -0,0 +1,7 @@
+
+function showStuff(id, btn) {
+  $("#A,#B").hide(); // hide both each time
+
+  document.getElementById(id).style.display = 'block';
+
+}
---
+++ Client_Project/src/Controller/Checkout_controller.java
@@ -0,0 +1,42 @@
+
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Checkout_controller
+ */
+@WebServlet("/checkout")
+public class Checkout_controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Checkout_controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("page", "checkout");
+		request.getRequestDispatcher("decorators/web.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
