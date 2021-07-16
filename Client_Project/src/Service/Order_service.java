196e3e0e4a30329ced40522ddf814a121eaccf9d
SiroBank999 <SiroBank999@gmail.com>
2021-07-16 16:25:50 +0700

    XONG DAT HANG[THÊM VÀO ORDER VÀ ORDER DETAIL]

--- Client_Project/src/Service/Order_service.java
+++ Client_Project/src/Service/Order_service.java
@@ -3,14 +3,36 @@ package Service;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Order;
 
 
 public class Order_service {
 	Connection conn = null;
 	PreparedStatement ps = null;
 	ResultSet rs = null;
-	public void insertOrder(String fullname, String phone,String email,String address,int id_user,double into ) {
-		String query ="INSERT INTO [order](fullname,phone,email,[address],id_user,[into]) VALUES(?,?,?,?,?,?)";
+	public List<Order> getListOrder(){
+		List<Order> list = new ArrayList<Order>();
+		String query = "SELECT * FROM [order]";
+		try {
+			conn =Database.Connect();
+			ps =conn.prepareStatement(query);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				Order order = new Order();
+				order.setId(rs.getString(1));//Chú ý lấy cột ID
+				list.add(order);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return list;
+		
+	}
+	public void insertOrder(String fullname, String phone,String email,String address,int id_user,double into,String id ) {
+		String query ="INSERT INTO [order](fullname,phone,email,[address],id_user,[into],id) VALUES(?,?,?,?,?,?,?)";
 		try {
 			conn =Database.Connect();
 			ps =conn.prepareStatement(query);
@@ -20,6 +42,7 @@ public class Order_service {
 			ps.setString(4, address);
 			ps.setInt(5, id_user);
 			ps.setDouble(6, into);
+			ps.setString(7, id);
 			ps.executeUpdate();
 			
 		} catch (Exception e) {
@@ -27,7 +50,7 @@ public class Order_service {
 		}
 	}
 	public static void main(String[] agrs) {
-		Order_service order = new Order_service();
-		order.insertOrder("Nguyễn Văn Nhơn","0333333","po@po","Tổ dân phố chầm",1,500000);
+		
+	
 	}
 }
